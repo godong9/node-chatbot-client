fde01ed4e7d2b2a4a55c3bfc9a9dc2c62702d5b7
GoDonghyun <godong9@gmail.com>
2017-02-12 00:49:25 +0900

    Update http-util

--- client/static/http-util.js
+++ client/static/http-util.js
@@ -1,12 +1,17 @@
+const HOST_PATH = 'https://dadocbot.herokuapp.com';
+
 function HttpUtil() {
 
 }
 
 HttpUtil.getData = function(url, params, callback) {
   $.ajax({
-    url: url,
+    url: HOST_PATH + url,
     type: 'GET',
     data: params,
+    headers: {
+      authorization: "bearer " + Document.cookie && Document.cookie.accessToken
+    },
     error: function errorHandler(jqXHR) {
       alert(jqXHR.responseText);
       callback(jqXHR.responseText || "Error");
@@ -19,11 +24,14 @@ HttpUtil.getData = function(url, params, callback) {
 
 HttpUtil.postData = function(url, params, callback) {
   $.ajax({
-    url: url,
+    url: HOST_PATH + url,
     type: 'POST',
     contentType: 'application/json;charset=UTF-8',
     dataType: 'json',
     data: JSON.stringify(params),
+    headers: {
+      authorization: "bearer " + Document.cookie && Document.cookie.accessToken
+    },
     error: function errorHandler(jqXHR) {
       alert(jqXHR.responseText);
       callback(jqXHR.responseText || "Error");
@@ -36,11 +44,14 @@ HttpUtil.postData = function(url, params, callback) {
 
 HttpUtil.putData = function(url, params, callback) {
   $.ajax({
-    url: url,
+    url: HOST_PATH + url,
     type: 'PUT',
     contentType: 'application/json;charset=UTF-8',
     dataType: 'json',
     data: JSON.stringify(params),
+    headers: {
+      authorization: "bearer " + Document.cookie && Document.cookie.accessToken
+    },
     error: function errorHandler(jqXHR) {
       alert(jqXHR.responseText);
       callback(jqXHR.responseText || "Error");
@@ -53,11 +64,14 @@ HttpUtil.putData = function(url, params, callback) {
 
 HttpUtil.deleteData = function(url, params, callback) {
   $.ajax({
-    url: url,
+    url: HOST_PATH + url,
     type: 'DELETE',
     contentType: 'application/json;charset=UTF-8',
     dataType: 'json',
     data: JSON.stringify(params),
+    headers: {
+      authorization: "bearer " + Document.cookie && Document.cookie.accessToken
+    },
     error: function errorHandler(jqXHR) {
       alert(jqXHR.responseText);
       callback(jqXHR.responseText || "Error");
