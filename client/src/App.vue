d53cad0bc001b56c6ab17952c0b802ccaa973201
dhy039@gmail.com <eh03090306>
2017-02-11 20:30:07 +0900

    login and coach vue layout complete

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <login></login>
   </div>
 </template>
 
 <script>
 import Login from './components/Login';
+
 
 export default {
   name: 'app',
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="login">
-    <h1>{{ msg }}</h1>
-    <h2>Login Page</h2>
-  </div>
+<div id="background_gradient">
+ <!-- comment container -->
+    <div id="comment_container">
+        <p class="text-center">당신만을 위한 <b>코치와</b>
+            <br> <b>다이어트</b>를 함께하세요!</p>
+    </div>
+    <!-- coach image container -->
+    <div id="coach_image_container">
+        <img src="../assets/images/login_img_cute.png" />
+    </div>
+    <!-- facebook login container -->
+    <div id="facebook_login_container">
+        <img id="login_facebook" src="../assets/images/login_btn_fb.png" v-on:click="fb_login"/>
+        <p class="text-center">개인의 동의없이 페이스북에 게시되지 않습니다.</p>
+    </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'login',
+  name: 'coach',
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
     };
   },
 };
+
+new Vue({
+methods:{
+fb_login:function(){
+alert('fb_login');
+
+}
+}
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
