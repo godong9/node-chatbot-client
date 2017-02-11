c6906ba932f53989e2e9ac84b21fbdae968e2ac4
GoDonghyun <godong9@gmail.com>
2017-02-11 17:31:11 +0900

    Update routes

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,22 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
 
-/* eslint-disable no-new */
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: '<p>home page</p>' }
+const About = { template: '<p>about page</p>' }
+const routes = {
+  '/': Home,
+  '/about': About
+}
+
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App },
-});
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
