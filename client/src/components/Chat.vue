7ce2b4fe4572fafb2ace5fa1159951db8b8e9061
GoDonghyun <godong9@gmail.com>
2017-02-17 11:19:19 +0900

    Update Chat page

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -5,21 +5,21 @@
         </nav>
         <div id="chat_container">
             <div v-for="item in chat_histories" class="row chat-item">
-              <div v-if="item.coach_type > 0" class="coach-chat">
+              <div v-if="item.is_user" class="user-chat">
+                <div class="my-img-item col-xs-4 col-md-4">
+                </div>
+                <div class="talk-bubble tri-right border round right-in col-xs-7 col-md-7">
+                  {{item.text}}
+                </div>
+                <div class="col-xs-1 col-md-1">
+                </div>
+              </div>
+              <div v-else class="coach-chat">
                 <div class="coach-img-item col-xs-3 col-md-3">
                     <img class="coach-img img-circle" :src="item.coach_img">
                 </div>
                 <div class="talk-bubble tri-right border round left-in col-xs-8 col-md-8">
                     {{item.text}}
-                </div>
-                <div class="col-xs-1 col-md-1">
-                </div>
-              </div>
-              <div v-else class="user-chat">
-                <div class="my-img-item col-xs-4 col-md-4">
-                </div>
-                <div class="talk-bubble tri-right border round right-in col-xs-7 col-md-7">
-                  {{item.text}}
                 </div>
                 <div class="col-xs-1 col-md-1">
                 </div>
@@ -48,59 +48,58 @@
   let is_sending = false;
   let current_message = '';
   let coach_name = '무서운형';
-  let chat_histories = [
-    {
-      coach_type: 1,
-      coach_img: '/static/images/coach_1_angry.gif',
-      text: '가아너힘ㄴ어하ㅣ먼ㅇ하ㄴㅁㅇㅎㅁㄴㅇㅎgdsgadsㅣㅓㅁㄴ아ㅣ험닝허ㅣㅁㄴㅇ허ㅣㅏㅁㄴ어힘넝히ㅏ먼이ㅏ험닝허미낭ㅎ'
-    },
-    {
-      coach_type: 2,
-      coach_img: '/static/images/coach_1_angry.gif',
-      text: '코치대화2'
-    },
-    {
-      coach_type: 3,
-      coach_img: '/static/images/coach_1_angry.gif',
-      text: '코치대화3'
-    },
-    {
-      text: '유저대화'
-    },
-    {
-      text: '유저대화'
-    },
-  ];
+  let chat_histories = [];
+  let coach_hi_message = {
+    type: 1,
+    emotion: 'hi',
+    text: '안녕, 반가워!'
+  };
   let commands = [
     {
       cmd: '식사 기록'
     },
     {
-      cmd: '식사 조회'
+      cmd: '식사 보기'
     },
     {
       cmd: '운동 기록'
     },
     {
-      cmd: '운동 조회'
+      cmd: '운동 보기'
     },
     {
       cmd: '몸무게 기록'
     },
     {
-      cmd: '몸무게 조회'
+      cmd: '몸무게 보기'
     },
     {
       cmd: '미션 기록'
     },
     {
-      cmd: '미션 조회'
+      cmd: '미션 보기'
     },
   ];
 
   export default {
     name: 'chat',
     data() {
+      let self = this;
+      let i = 0;
+      HttpUtil.getData('/messages', {}, function(err, data) {
+        for (i = 0; i < data.messages.length; i++) {
+          let item = data.messages[i];
+          if (item.isUser) {
+            self.add_user_history(item.text);
+          } else {
+            item.type = 2;
+            item.emotion = 'like';
+            self.add_coach_history(item);
+          }
+        }
+        self.add_coach_history(coach_hi_message);
+      });
+
       return {
         is_sending: is_sending,
         current_message: current_message,
@@ -111,6 +110,7 @@
     },
     methods: {
       send_message() {
+        let self = this;
         if (!this.current_message) {
           return false;
         }
@@ -118,25 +118,40 @@
           alert("요청 중입니다. 잠시 후 다시 시도해주세요!");
           return false;
         }
+        let userMessage = this.current_message;
+        self.add_user_history(userMessage);
 
-        this.add_user_history(this.current_message);
+        self.current_message = '';
+        self.is_sending = true;
 
-        this.current_message = '';
-        //TODO: is_sending 트루로 변경하고 서버로 데이터 전송 후 응답오면 is_sending 팔스로 변경
-        this.is_sending = true;
-        this.is_sending = false;
-        $('#chat_container').animate({ scrollTop: $('#chat_container').height() }, 100);
+        HttpUtil.postData('/messages/ask', {text: userMessage}, function(err, data) {
+          self.is_sending = false;
+          data.type = 2;
+          data.emotion = 'like';
+
+          self.add_coach_history(data);
+          self.scroll_to_bottom();
+        });
       },
       add_user_history(user_message) {
         this.chat_histories.push({
+          is_user: true,
           text: user_message
         });
+        this.scroll_to_bottom();
       },
       add_coach_history(coach_message) {
+        coach_message.img = '/static/images/coach_' + coach_message.type + '_' + coach_message.emotion + '.gif';
         this.chat_histories.push({
           coach_type: coach_message.type,
-          coach_img: coach_message.url,
+          coach_img: coach_message.img,
           text: coach_message.text
+        });
+        this.scroll_to_bottom();
+      },
+      scroll_to_bottom() {
+        this.$nextTick(() => {
+          $('#chat_container').animate({ scrollTop: $('#chat_container').prop("scrollHeight") }, 500);
         });
       },
       click_cmd(event) {
